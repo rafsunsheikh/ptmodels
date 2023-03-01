10b501ec6e1c9cf06f56ec6cb327db15aca407a3
rafsunsheikh <rafsun.sheikh@gmail.com>
2023-03-01 15:35:32 +0600

    0.0.2

--- setup.py
+++ setup.py
@@ -1,34 +1,32 @@
-from setuptools import setup, find_packages
-import codecs
-import os
 
-here = os.path.abspath(os.path.dirname(__file__))
-
-with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
-    long_description = "\n" + fh.read()
-
-VERSION = '0.0.2'
-DESCRIPTION = 'Image classification using Pre-trained Models'
-LONG_DESCRIPTION = 'A package that allows to classifiy image dataset using pre-trained models and helps understand which model works best.'
-
-# Setting up
+from distutils.core import setup
 setup(
-    name="ptmodels",
-    version=VERSION,
-    author="MD Rafsun Sheikh",
-    author_email="<201614123@student.mist.ac.bd>",
-    description=DESCRIPTION,
-    long_description_content_type="text/markdown",
-    long_description=long_description,
-    packages=find_packages(),
-    install_requires=['numpy', 'pandas', 'tensorflow', 'keras', 'scikit-learn'],
-    keywords=['python', 'image', 'classification', 'pre-trained model', 'api', 'evaluation'],
-    classifiers=[
-        "Development Status :: 1 - Planning",
-        "Intended Audience :: Developers",
-        "Programming Language :: Python :: 3",
-        "Operating System :: Unix",
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: Microsoft :: Windows",
-    ]
+  name = 'ptmodels',         # How you named your package folder (MyLib)
+  packages = ['ptmodels'],   # Chose the same as "name"
+  version = '0.0.2',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'Pre-trained models image classifier',   # Give a short description about your library
+  author = 'MD Rafsun sheikh',                   # Type in your name
+  author_email = '201614123@student.mist.ac.bd',      # Type in your E-Mail
+  url = 'https://github.com/rafsunsheikh/ptmodels',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/rafsunsheikh/ptmodels/archive/refs/tags/0.0.2.tar.gz',    # I explain this later on
+  keywords = ['Image', 'Classification', 'Pre-trained Model'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+          'numpy',
+          'pandas',
+          'tensorflow',
+          'sklearn',
+          'keras',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.10',
+  ],
 )
